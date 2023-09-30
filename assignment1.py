f48b8d7541baf465eda24e1bc1239ca4638b2952
Octavio Sanchez <octavio.a.sanchez@gmail.com>
2023-09-30 00:58:17 -0400

    finished assingment 1

--- assignment1.py
+++ assignment1.py
@@ -1,4 +1,21 @@
 import pandas as pd
-import bs4 as BeautifulSoup
+import requests
 
-url = 
+url = 'https://api.covidtracking.com/v1/us/daily.csv'
+
+response = requests.get(url)
+
+df = pd.read_csv(url)
+
+cols_to_keep = ['date', 'hospitalizedCumulative']
+
+for col in df.columns:
+    if col not in cols_to_keep:
+        df.drop(columns=col, inplace=True)
+
+print(df)
+
+first_hospitalizedCumulative = df.iloc[-1, 1]
+
+last_hospitalizedCumulative = df.loc['hospitalizedCumulative',1]
+print(last_hospitalizedCumulative - first_hospitalizedCumulative)
