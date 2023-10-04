d2040faebaaf63af1bfee99712c2e47bf4605449
Octavio Sanchez <octavio.a.sanchez@gmail.com>
2023-10-04 14:30:49 -0400

    misc edits

--- code/in_class_exercise6.py
+++ code/in_class_exercise6.py
@@ -1,8 +1,9 @@
 import json
 import pandas as pd
 
-data = pd.read_json('\Data\imdb_movies_2000to2022.prolific.json')
+data = pd.read_json('Data/imdb_movies_2000to2022.prolific.json', lines=True)
 
 df = pd.DataFrame(data)
 
-df.head()
+actor_genre_df = df[['genres']].explode('actors')
+print(actor_genre_df.head())
