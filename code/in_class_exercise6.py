28041eaef3e565ebfbd6e016b12a8b3c60dfb95c
bigboioct <43962896+bigboioct@users.noreply.github.com>
2023-10-11 12:52:30 -0400

    misc edits

--- code/in_class_exercise6.py
+++ code/in_class_exercise6.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import json
+import numpy as np
 from sklearn.metrics import DistanceMetric
 
 data = []
-with open('C:/Users/octav/Documents/GitHub/INST414/Data/imdb_movies_2000to2022.prolific.json') as f:
+with open('Data/imdb_movies_2000to2022.prolific.json') as f:
     for line in f:
-        movie = json.loads(line)
-        for actor in movie['actors']:
+        entry = json.loads(line)
+        for actor in entry['actors']:
             row = {'actor': actor[0]}
-            for genre in movie['genres']:
+            for genre in entry['genres']:
                 if genre not in row:
                     row[genre] = 0
                 row[genre] += 1
